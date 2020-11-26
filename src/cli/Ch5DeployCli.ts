cb5d7eecb8d8c5a8c99d305d5eb38047c79792d4
Razvan Sandu <razvan.sandu@corebuild.eu>
2020-11-26 11:23:55 +0200

    fix(deploy): mask passphrase

--- src/cli/Ch5DeployCli.ts
+++ src/cli/Ch5DeployCli.ts
@@ -140,7 +140,7 @@ export class Ch5DeployCli {
             name: 'user',
           },
           {
-            type: 'passphrase',
+            type: 'password',
             message: 'Enter passphrase for the private key (empty for no passphrase)',
             name: 'passphrase',
             mask: '*'
